ac2321ffef2764037e971dab3be6df399ded6b40
leabeaulieu <100712728+leabeaulieu@users.noreply.github.com>
2022-11-29 14:34:45 +0100

    update project

--- projet.py
+++ projet.py
@@ -2,26 +2,11 @@ import gzip
 
 file_1 = "C:/Users/leabe/OneDrive/Bureau/ALG/Ecoli_100Kb/ecoli_100Kb_reads_5x.fasta"
 
-def remove_header(input_file, output_file):
-    with open(input_file, "r") as ifh:
-        with open(output_file, "w") as ofh:
-            first_line = True
-            for line in ifh:
-                if ">" not in ifh:
-                    ofh.write(line[:-1])
-                else:
-                    if first_line:
-                        first_line = False
-                    else:
-                        ofh.write("\n")
-
 def triplet(file):
     words = ["AAA", "AAC", "AAG", "AAT", "ACA", "AGA", "ATA", "ACC", "ACG", "ACT", "AGC", "AGG", "AGT", "ATC", "ATG", "ATT"]
     dict_triplet = dict()
     with open(file, "r") as file_reader:
-        #count_total = 0
         for line in file_reader:
-            #count_total += 1
             line = line.strip("\n")
             if ">" not in line:
                 i=0
@@ -36,38 +21,11 @@ def triplet(file):
                         i+=1
     return dict_triplet
 
-dict_read = triplet(file_1)
-count=0
-for i in dict_read.values() :
-    print(len(i))
-print(count)
-
-
-def triplet_moche(file):
-    triplet_A = []
-    triplet_C = []
-    triplet_G = []
-    triplet_T = []
-    all_triplets = []
-    with open(file, "r") as file_reader:
-        for line in file_reader:
-            line = line.strip("\n")
-            if ">" not in line:
-                if "AAA" in line :
-                    triplet_A.append(line)
-                elif "CCC" in line :
-                    triplet_C.append(line)
-                elif "GGG" in line :
-                    triplet_G.append(line)
-                elif "TTT" in line :
-                    triplet_T.append(line)
-                for i in triplet_A and triplet_C and triplet_G and triplet_T: 
-                    if i not in all_triplets: 
-                        all_triplets.append(i)         
-    return all_triplets
-                
-
-            
-
-remove_header(file_1, "C:/Users/leabe/OneDrive/Bureau/ALG/Ecoli_100Kb/clean_file.txt")
-#print(triplet(file_1))
+def write_file(input_file, output_name):
+    with open (output_name,"w") as out :
+        dico_read = triplet(input_file)
+        sorted_dict = {key: value for key, value in sorted(dico_read.items())}
+        for i in sorted_dict.values():
+            for j in i:
+                out.write(j)
+                out.write("\n")
